6a6b7333a82d9768a6da12429c237168604173c5
Tony <1414927+zfbTony@users.noreply.github.com>
2018-07-28 02:03:44 -0400

    opps, wrong module

--- commands/fun/daily.js
+++ commands/fun/daily.js
@@ -50,7 +50,7 @@ exports.info = {
     name: "daily",
     aliases: ['dailies'],
     usage: ['daily'],
-    module: "Fun",
+    module: "Xp",
     serverOnly: false,
     ownerOnly: false,
     requires: [],
