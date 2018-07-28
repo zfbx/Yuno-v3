76f9ebf2c4c6d1f255b9d646640da4f27bc8dc00
Tony <1414927+zfbTony@users.noreply.github.com>
2018-07-28 01:55:24 -0400

    Strange bug with discord api?

--- events/warn.js
+++ events/warn.js
@@ -1,6 +1,6 @@
 module.exports = (client, warn) => {
     if (warn) { //apparently this check has to be made? *If I wanna make sure no token is involved.
-        client.log('WARN', warn.replace(client.token, '[TOKEN-HERE]'))
+        client.log('WARN', String(warn).replace(client.token, '[TOKEN-HERE]'))
     } else {
         client.log('WARN', 'There was.. some kind of warning apparently.');
     }
