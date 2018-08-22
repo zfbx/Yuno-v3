a8ac39930d419a9fa9d463ac759db1e3aca00506
Tony <1414927+zfbTony@users.noreply.github.com>
2018-08-22 00:20:13 -0400

    Added jokes command
    With a little under 200 jokes (will try to add more later)

---
+++ commands/fun/joke.js
@@ -0,0 +1,18 @@
+const jokes = require('../../data/jokes.json');
+const rand = require('../../functions/random.js');
+
+exports.run = async (client, message, args) => {
+    message.channel.send(`${jokes[rand(0, jokes.length)]}`);
+};
+
+exports.info = {
+    name: 'joke',
+    aliases: [],
+    usage: ['joke'],
+    module: "Fun",
+    serverOnly: false,
+    ownerOnly: false,
+    requires: [],
+    botPermissions: ['SEND_MESSAGES'],
+    description: "Have me tell a random joke."
+};
