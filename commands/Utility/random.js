9cff1141bc7f85700c532260ded5eced704be9b6
Tony <1414927+zfbTony@users.noreply.github.com>
2018-11-16 05:53:00 -0500

    added implied order reversing and implied minimum value

--- commands/Utility/random.js
+++ commands/Utility/random.js
@@ -5,8 +5,10 @@ exports.run = async (client, message, args) => {
         if (parseInt(args[0]) < parseInt(args[1])) {
             return message.channel.send(`Your random number between ${args[0]} and ${args[1]} is: ${rand(parseInt(args[0]), parseInt(args[1]))}`);
         } else {
-            return message.channel.send('Please make sure the min number is first and max number is second');
+            return message.channel.send(`Your random number between ${args[1]} and ${args[0]} is: ${rand(parseInt(args[1]), parseInt(args[0]))}`);
         }
+    } else if (args[0]) {
+        return message.channel.send(`Your random number between 1 and ${args[0]} is: ${rand(1, parseInt(args[0]))}`);
     } else {
         return message.channel.send('Please supply a minimum number and maximum number');
     }
