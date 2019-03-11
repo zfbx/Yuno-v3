291be882d7d6dffd5d5ebbf4adcebfc618cf85a8
Tony <1414927+zfbTony@users.noreply.github.com>
2019-03-11 01:11:27 -0400

    fixed error in guild delete error

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,14 +1,15 @@
 const Discord = require('discord.js');
 
 module.exports = (client, guild) => {
-    client.fetchUser(client.config.ownerid[0]).then( user => {
+    client.users.fetch(client.config.ownerid[0]).then( user => {
         const embed = new Discord.MessageEmbed()
         .setColor(client.config.embedcolor)
         .setAuthor('Server Deleted/Left')
         .addField('Name', `${guild.name}`)
         .addField('ID', `${guild.id}`)
         .addField('Owner', `${guild.owner.user.tag}`)
-        .addField('Members', `${guild.memberCount}`);
+        .addField('Members', `${guild.memberCount}`)
+        .setFooter(new Date().toUTCString());
         user.send({ embed }).catch(console.error);
     });
     client.log.info(`I have been from the guild: ${guild.name}, owned by: ${guild.owner.user.tag}, with ${guild.memberCount} members.`);
