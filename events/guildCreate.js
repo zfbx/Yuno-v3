3dafd8f032b8b86668474d028d86683d60f82a2d
Tony <1414927+zfbTony@users.noreply.github.com>
2019-03-11 01:06:44 -0400

    Added GuildCreation logging

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,20 +1,18 @@
+const Discord = require('discord.js');
+
 module.exports = (client, guild) => {
-    client.fetchUser(client.config.ownerid[0]).then( user => {
+    client.users.fetch(client.config.ownerid[0]).then( user => {
         const embed = new Discord.MessageEmbed()
-        .setColor(client.config.embedcolor)
-        .setAuthor('Server Joined')
-        .addField('Name', `${guild.name}`)
-        .addField('ID', `${guild.id}`)
-        .addField('Owner', `${guild.owner.user.tag}`)
-        .addField('Members', `${guild.memberCount}`);
+            .setColor(client.config.embedcolor)
+            .setAuthor('Server Joined')
+            .addField('Name', `${guild.name}`)
+            .addField('ID', `${guild.id}`)
+            .addField('Owner', `${guild.owner.user.tag}`)
+            .addField('Members', `${guild.memberCount}`)
+            .setFooter(new Date().toUTCString());
         user.send({ embed }).catch(console.error);
     });
     client.log.info(`I have been added to the guild: ${guild.name}, owned by: ${guild.owner.user.tag}, with ${guild.memberCount} members.`);
 
-    client.ensure(guild.id, {
-        prefix: client.config.prefix,
-        logChannel: "",
-        welcomeChannel: "",
-        welcomeMessage: "Hey {{user}}, Welcome to {{guild}}!"
-    })
+    client.guildDB.ensure(guild.id, client.guildDBDefaults);
 }
