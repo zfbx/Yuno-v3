0a8b624f59970e7c25c593c7de6478e42b1fcfb2
Tony <1414927+zfbTony@users.noreply.github.com>
2019-03-11 01:18:31 -0400

    added extra events and commented spammy logging

--- app.js
+++ app.js
@@ -10,8 +10,26 @@ const client = new Discord.Client({
     ]
 });
 
-client.guildDB = new Enmap({ name: "guildDB" });
-client.twitchDB = new Enmap({ name: "twitchDB" });
+client.guildDB = new Enmap({ 
+    name: "guildDB",
+    fetchAll: false,
+    autoFetch: true,
+    cloneLevel: 'deep' 
+});
+//client.twitchDB = new Enmap({ name: "twitchDB" });
+
+client.guildDBDefaults = {	
+    prefix: ".",
+    logChannel: "",
+    logPics: true,
+    logNames: true,
+    logEdits: true,
+    logRoles: true,
+    logVoice: false,
+    autoRole: "",
+    welcomeChannel: "",
+    welcomeMessage: "Say hello to {{user}}, everyone!"
+}
 
 client.config = require('./config.js');
 client.log = logger({
@@ -29,7 +47,7 @@ function loadCommands(folder) {
         client.log.info(`${files.length} ${folder} commands found.`);
         files.forEach(f => {
             let props = require(`./${f}`);
-            client.log.debug(`Loading Command: ${props.info.name}.`);
+            //client.log.debug(`Loading Command: ${props.info.name}.`); //very spammy with lots of commands
             client.commands.set(props.info.name, props);
             props.info.aliases.forEach(alias => {
                 client.aliases.set(alias, props.info.name);
@@ -55,10 +73,13 @@ client.on('message', message => require('./events/message')(client, message));
 client.on('messageUpdate', (oldMessage, newMessage) => require('./events/message')(client, newMessage, oldMessage));
 client.on('guildCreate', guild => require('./events/guildCreate')(client, guild));
 client.on('guildDelete', guild => require('./events/guildDelete')(client, guild));
+client.on('messageDelete', message => require('./events/messageDelete')(client, message));
 client.on('guildMemberAdd', member => require('./events/guildMemberAdd')(client, member));
+client.on('guildMemberUpdate', (oldMember, newMember) => require('./events/guildMemberUpdate')(client, oldMember, newMember));
 client.on('guildMemberRemove', member => require('./events/guildMemberRemove')(client, member));
 client.on('guildBanAdd', (guild, user) => require('./events/guildBanAdd')(client, guild, user));
 client.on('guildBanRemove', (guild, user) => require('./events/guildBanRemove')(client, guild, user));
+//client.on('userUpdate', (oldUser, newUser) => require('./events/userUpdate')(client, oldUser, newUser));
 client.on('disconnect', () => require('./events/disconnect')(client));
 client.on('reconnecting', () => require('./events/reconnecting')(client));
 client.on('warn', warn => require('./events/warn')(client, warn));
