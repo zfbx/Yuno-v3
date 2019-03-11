890db048fe9b61258a1f6bb75e1ba7a1e421ca94
Tony <1414927+zfbTony@users.noreply.github.com>
2019-03-11 01:10:56 -0400

    added logging for user leave

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,4 +1,22 @@
+const Discord = require('discord.js');
+
 module.exports = (client, member) => {
     client.log.info(`${member.user.tag} (${member.id}) is no longer in ${member.guild.name} (${member.guild.id})`);
-    //member.guild.defaultChannel.send(`Please welcome ${member.user.username} to the server!`);
+    
+    client.guildDB.ensure(member.guild.id, client.guildDBDefaults);
+
+    var logChannelId = client.guildDB.get(member.guild.id, "logChannel");
+    if(logChannelId) {
+        logServer = member.guild.channels.get(logChannelId);
+        if (logServer) {
+            const embed = new Discord.MessageEmbed()
+                .setAuthor("User left")
+                .setColor(client.config.embedcolor)
+                .setImage(member.user.avatarURL({format: 'png', size: 2048}))
+                .addField("Id", member.user.tag)
+                .addField("Joined Discord", member.user.createdTimestamp, true)
+                .setFooter(new Date().toUTCString());
+            logServer.send({embed});
+        }
+    }
 }
