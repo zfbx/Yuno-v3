c4ce6e01f76c08696c0120a2df6ac50b8fa3fd8a
Tony <1414927+zfbTony@users.noreply.github.com>
2019-03-12 22:23:51 -0400

    -WIP

--- commands/Help/help.js
+++ commands/Help/help.js
@@ -3,7 +3,8 @@ exports.run = async (client, message, args) => {
     if (!args[0]) {
         const commandNames = Array.from(client.commands.keys());
         const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-        message.channel.send(`= Command List =\n\n[Use ${client.config.prefix}help <commandname> for details]\n\n${client.commands.map(c => `${client.config.prefix}${c.info.name}${' '.repeat(longest - c.info.name.length)} :: ${c.info.description}`).join('\n')}`, {code:'asciidoc'});
+        message.channel.send(`= Command List =\n\n[Use ${client.config.prefix}help <commandname> for details]\n
+            ${client.commands.map(c => `${client.config.prefix}${c.info.name}${' '.repeat(longest - c.info.name.length)} :: ${c.info.description}`).join('\n')}`, {code:'asciidoc'});
     } else {
         let command = args[0];
         if (client.commands.has(command)) {
