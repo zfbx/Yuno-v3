c7e9e08432a93a90c184befa211917256f658d3b
Tony <1414927+zfbTony@users.noreply.github.com>
2019-04-22 01:53:18 -0400

    fixed mistake with calling member instead of user

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -2,16 +2,16 @@ const Discord = require('discord.js');
 
 module.exports = (client, guild, user) => {
 
-    client.guildDB.ensure(member.guild.id, client.guildDBDefaults);
-    var logChannelId = client.guildDB.get(member.guild.id, "logChannel");
+    client.guildDB.ensure(guild.id, client.guildDBDefaults);
+    var logChannelId = client.guildDB.get(guild.id, "logChannel");
     if(logChannelId) {
-        logServer = member.guild.channels.get(logChannelId);
+        logServer = guild.channels.get(logChannelId);
         if (logServer) {
             const embed = new Discord.MessageEmbed()
                 .setAuthor("User Banned")
                 .setColor(client.config.embedcolor)
-                .setImage(member.user.avatarURL({format: 'png', size: 2048}))
-                .addField("Id", `${member.user.tag} (${member.user.id})`)
+                .setImage(user.avatarURL({format: 'png', size: 2048}))
+                .addField("Id", `${user.tag} (${user.id})`)
                 .setFooter(new Date().toUTCString());
             logServer.send({embed});
         }
