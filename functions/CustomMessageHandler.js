055f219541b783638a9f0e9bbd1c3a81c88679f7
Tony <1414927+zfbTony@users.noreply.github.com>
2019-04-23 06:46:58 -0400

    added custom server handler

--- functions/CustomMessageHandler.js
+++ functions/CustomMessageHandler.js
@@ -1,9 +1,18 @@
 module.exports = (client, message) => {
     var chid = message.channel.id;
+    var msg = message.content;
+    var uid = message.author.id;
     
     
     if (chid === "566980758107848711"){
-        if (message.content !== ".accept" && !client.config.ownerid.includes(message.author.id))
+        if (msg !== ".accept" && !client.config.ownerid.includes(uid))
             message.delete();
+    }
+
+    if (chid === "569276257817067560") {
+        if (uid !== "194794229849915394" && !client.config.ownerid.includes(uid)){
+            if(msg !== ".agree" || msg !== ".accept")
+                message.delete();
+        }
     }
 }
