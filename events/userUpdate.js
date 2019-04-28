a12ee9a0269ebf595a4ff52b02cdd3fdabb7245d
Tony <1414927+zfbTony@users.noreply.github.com>
2019-04-28 08:40:29 -0400

    Fixed event to work but only in DM

--- events/userUpdate.js
+++ events/userUpdate.js
@@ -2,29 +2,19 @@ const Discord = require('discord.js');
 const time = require('../functions/time');
 
 module.exports = (client, oldUser, newUser) => {
-    client.guildDB.ensure(newUser.guild.id, client.guildDBDefaults);
-    var logChannelId = client.guildDB.get(newUser.guild.id, "logChannel");
-    if(logChannelId) {
-        logServer = newUser.guild.channels.get(logChannelId);
-        if(logServer) {
-            if (oldUser.user.tag !== newUser.user.tag) { // check for username change
-                const embed = new Discord.MessageEmbed()
-                    .setAuthor("User Changed Name")
-                    .setColor(client.config.embedcolor)
-                    .addField(newUser.user.tag, `(${newUser.id})`)
-                    .addField("Old: ", oldUser.user.tag)
-                    .addField("New: ", newUser.user.tag)
-                    .setFooter(time.stamp());
-                logServer.send({embed});
+    //client.guildDB.ensure(newUser.guild.id, client.guildDBDefaults);
+    //var logChannelId = client.guildDB.get(newUser.guild.id, "logChannel");
 
-            }else {
-                const embed = new Discord.MessageEmbed()
-                    .setAuthor("Something changed for this user but I'm not sure what.")
-                    .setColor(client.config.embedcolor)
-                    .addField(newUser.user.tag, `(${newUser.id})`)
-                    .setFooter(time.stamp());
-                logServer.send({embed});
-            }
+    if (client.config.ownerlogging) { // Must log to a fixed guild OR DM
+        if (oldUser.tag !== newUser.tag) { // check for username change 
+            const embed = new Discord.MessageEmbed()
+                .setAuthor("User Tag Changed")
+                .setColor(client.config.embedcolor)
+                .addField(newUser.tag, `(${newUser.id})`)
+                .addField("Old: ", oldUser.tag)
+                .addField("New: ", newUser.tag)
+                .setFooter(time.stamp());
+            client.users.get(client.config.ownerid[0]).send({embed});
         }
     }
 }
